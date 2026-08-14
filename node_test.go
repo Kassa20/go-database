0da50cf77bd6d7b37bdf1b62eca22c059c870f69
ykassa <ykassa47@gmail.com>
2026-08-14 11:33:55 -0700

    adding tests for node insertion

--- node_test.go
+++ node_test.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	// "testing"
+	"testing"
 	"unsafe"
 )
 
@@ -27,6 +28,32 @@ func newMemoryTree() *BTree {
 		del: func(ptr uint64) {
 			delete(pages, ptr)
 		},
+	}
+}
+
+func TestLeafInsert(t *testing.T) {
+	old := BNode(make([]byte, BTREE_PAGE_SIZE))
+	old.setHeader(BNODE_LEAF, 2)
+
+	//keys
+	nodeAppendKV(old, 0, 0, []byte("key1"), []byte("hi"))
+	nodeAppendKV(old, 1, 0, []byte("key3"), []byte("hello"))
+
+	new := BNode(make([]byte, BTREE_PAGE_SIZE))
+	leafInsert(new, old, 1, []byte("key2"), []byte("there"))
+
+	if new.nkeys() != 3 {
+		t.Fatalf("nkeys = %d, want 3", new.nkeys())
+	}
+
+	want := []string{"key1", "key2", "key3"}
+	for i, w := range want {
+		if got := string(new.getKey(uint16(i))); got != w {
+			t.Errorf("key %d = %q, want %q", i, got, w)
+		}
+	}
+	if got := string(new.getVal(1)); got != "there" {
+		t.Errorf("val 1 = %q, want %q", got, "there")
 	}
 
 }
